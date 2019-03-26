f2989460a24328649a70b57330c8368fc66d90b2
lauraVzarco <laurav.zarco@gmail.com>
2019-03-26 15:05:45 +0100

    Updates

---
+++ precache-manifest.df356d57a42c3379786f6829097c06e8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ea0f7054d5a6dafcf9c",
+    "url": "/xpruebasportal/static/js/main.9d28b20f.chunk.js"
+  },
+  {
+    "revision": "3cd21294df88a991cd89",
+    "url": "/xpruebasportal/static/js/runtime~main.667be6f9.js"
+  },
+  {
+    "revision": "3e5d3a9c61077b671855",
+    "url": "/xpruebasportal/static/js/2.eab55d86.chunk.js"
+  },
+  {
+    "revision": "c26f37a5aed43752800cf1d046e9f893",
+    "url": "/xpruebasportal/index.html"
+  }
+];
