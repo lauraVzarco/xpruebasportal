0a2626106ae4351c44abedd99d48d03317478931
lauraVzarco <laurav.zarco@gmail.com>
2019-03-26 15:14:34 +0100

    deploy adalab mode

---
+++ docs/precache-manifest.54680af5e2dfb7ca814af7581dbb4ac6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "20a1de14461bccbfc709",
+    "url": "./static/js/main.44551b25.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3e5d3a9c61077b671855",
+    "url": "./static/js/2.eab55d86.chunk.js"
+  },
+  {
+    "revision": "57970338b15d498d74f81cc8d56b7dd1",
+    "url": "./index.html"
+  }
+];
